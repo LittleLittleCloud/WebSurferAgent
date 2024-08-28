4e9e3a933fd968c5d6e25399d234983455f9f278
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-08-27 23:41:41 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using AutoGen.Core;
+using Azure.AI.OpenAI;
+using Microsoft.Playwright;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using webSurfurAgent;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new ()
+{
+    Headless = false,
+    SlowMo = 500,
+});
+
+var aoaiEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new Exception("AZURE_OPENAI_ENDPOINT is not set");
+var aoaiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY") ?? throw new Exception("AZURE_OPENAI_KEY is not set");
+
+var openaiClient = new AzureOpenAIClient(new Uri(aoaiEndpoint), new Azure.AzureKeyCredential(aoaiKey));
+var webSurferAgent = new WebSurfurAgent(openaiClient.GetChatClient("gpt-4o"), browser);
+var task = """
+    Can I disable parallel function call in gpt-4o? 
+    """;
+await webSurferAgent.SendAsync(task);
+
+//var page = await browser.NewPageAsync();
+//await page.GotoAsync("https://playwright.dev/dotnet");
+
+//// evaluate the page
+//var scriptPath = "page_script.js";
+//var script = File.ReadAllText(scriptPath);
+//await page.EvaluateAsync(script);
+
+//var interactiveRects = await page.EvaluateAsync<InteractiveRectangles>("MultimodalWebSurfer.getInteractiveRects();");
+//var visualViewport = await page.EvaluateAsync<VisualViewport>("MultimodalWebSurfer.getVisualViewport();");
+//var drawRects = await page.EvaluateAsync<Rectangle[]>("MultimodalWebSurfer.drawVisibleInteractiveRects();");
+//Console.WriteLine($"Interactive element count: {interactiveRects.Rects.Count()}");
+
+//Console.WriteLine($"Visual viewport: {JsonSerializer.Serialize(visualViewport)}");
+
+//// save a screenshot
+//await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+
+public class Element
+{
+    [JsonPropertyName("$id")]
+    public string Id { get; set; } = null!;
+}
+
+public class Rectangle : Element
+{
+    [JsonPropertyName("x")]
+    public double X { get; set; }
+
+    [JsonPropertyName("y")]
+    public double Y { get; set; }
+
+    [JsonPropertyName("width")]
+    public double Width { get; set; }
+
+    [JsonPropertyName("height")]
+    public double Height { get; set; }
+
+    [JsonPropertyName("top")]
+    public double Top { get; set; }
+
+    [JsonPropertyName("right")]
+    public double Right { get; set; }
+
+    [JsonPropertyName("bottom")]
+    public double Bottom { get; set; }
+
+    [JsonPropertyName("left")]
+    public double Left { get; set; }
+
+    [JsonPropertyName("element_id")]
+    public string ElementId { get; set; } = null!;
+
+    [JsonPropertyName("interactive_id")]
+    public int InteractiveId { get; set; }
+
+    [JsonPropertyName("content")]
+    public string Content { get; set; } = null!;
+}
+
+public class InteractiveRectangle : Element
+{
+    [JsonPropertyName("tag_name")]
+    public string TagName { get; set; } = null!;
+
+    [JsonPropertyName("role")]
+    public string Role { get; set; } = null!;
+
+    [JsonPropertyName("aria-name")]
+    public string AriaName { get; set; } = null!;
+
+    [JsonPropertyName("v-scrollable")]
+    public bool VScrollable { get; set; }
+
+    [JsonPropertyName("rects")]
+    public Rectangle[] Rects { get; set; } = Array.Empty<Rectangle>();
+}
+
+public class InteractiveRectangles : Element
+{
+    [JsonPropertyName("rects")]
+    public InteractiveRectangle[] Rects { get; set; } = Array.Empty<InteractiveRectangle>();
+}
+
+public class VisualViewport : Element
+{
+    [JsonPropertyName("width")]
+    public double Width { get; set; }
+
+    [JsonPropertyName("height")]
+    public double Height { get; set; }
+
+    [JsonPropertyName("offsetTop")]
+    public double OffsetTop { get; set; }
+
+    [JsonPropertyName("offsetLeft")]
+    public double OffsetLeft { get; set; }
+
+    [JsonPropertyName("pageTop")]
+    public double PageTop { get; set; }
+
+    [JsonPropertyName("pageLeft")]
+    public double PageLeft { get; set; }
+
+    [JsonPropertyName("scale")]
+    public double Scale { get; set; }
+
+    [JsonPropertyName("clientWidth")]
+    public double ClientWidth { get; set; }
+
+    [JsonPropertyName("clientHeight")]
+    public double ClientHeight { get; set; }
+
+    [JsonPropertyName("scrollWidth")]
+    public double ScrollWidth { get; set; }
+
+    [JsonPropertyName("scrollHeight")]
+    public double ScrollHeight { get; set; }
+}
+
---
+++ WebSurfurAgent.cs
@@ -0,0 +1,284 @@
+﻿using AutoGen.Core;
+using AutoGen.OpenAI;
+using AutoGen.OpenAI.Extension;
+using Azure.AI.OpenAI;
+using Microsoft.Playwright;
+using OpenAI;
+using OpenAI.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace webSurfurAgent;
+
+public partial class WebSurfurAgent : IAgent
+{
+    private readonly IAgent _agent;
+    private readonly IBrowser _browser;
+    private readonly IPage _page;
+    private readonly int _maxSteps = 10;
+
+    internal WebSurfurAgent(IAgent agent, IBrowser browser, int maxSteps = 10)
+    {
+        _agent = agent;
+        _browser = browser;
+        _page = browser.NewPageAsync().Result;
+    }
+
+    public WebSurfurAgent(ChatClient openaiClient, IBrowser browser, string name = "web-surfur", int maxSteps = 10)
+    {
+        var functionCallMiddleware = new FunctionCallMiddleware(
+            functions: [
+                this.VisitUrlFunctionContract,
+                this.TypeFunctionContract,
+                this.HistoryBackFunctionContract,
+                this.ClickFunctionContract,
+                this.PageDownFunctionContract,
+                this.PageUpFunctionContract,
+                this.SearchGoogleFunctionContract,
+                this.EnterFunctionContract,
+                ],
+            functionMap: new Dictionary<string, Func<string, Task<string>>>
+            {
+                [nameof(VisitUrl)] = this.VisitUrlWrapper,
+                [nameof(HistoryBack)] = this.HistoryBackWrapper,
+                [nameof(Click)] = this.ClickWrapper,
+                [nameof(Type)] = this.TypeWrapper,
+                [nameof(PageDown)] = this.PageDownWrapper,
+                [nameof(PageUp)] = this.PageUpWrapper,
+                [nameof(SearchGoogle)] = this.SearchGoogleWrapper,
+                [nameof(Enter)] = this.EnterWrapper,
+            });
+
+        _agent = new OpenAIChatAgent(
+            chatClient: openaiClient,
+            name: name)
+            .RegisterMessageConnector()
+            .RegisterMiddleware(functionCallMiddleware)
+            .RegisterPrintMessage();
+
+
+        _browser = browser;
+        _page = browser.NewPageAsync().Result;
+    }
+
+    public string Name => _agent.Name;
+
+    public async Task<IMessage> GenerateReplyAsync(IEnumerable<IMessage> messages, GenerateReplyOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        var taskPrompt = """
+            summarize the task from chat history.
+            """;
+
+        var task = await _agent.SendAsync(taskPrompt, messages);
+
+        var remainingSteps = _maxSteps;
+        var prompt = $"""
+            Resolve the given task by browsing the web. Suggest one step at each time. Use page up or page down when the page is not fully visible.
+
+            # Task
+            {task.GetContent() ?? throw new Exception("task is empty")}
+
+            If you gather enough information to answer the task, you can stop browsing and provide the answer using the following format
+            ```answer
+            // your answer
+            ```
+            """;
+
+        var promptMessage = new TextMessage(Role.User, prompt);
+
+        var chatHistory = new List<IMessage>()
+        {
+            promptMessage
+        };
+        while (remainingSteps > 0)
+        {
+            var pageSnapshot = await _page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+            var image = File.ReadAllBytes("screenshot.png");
+            var imageMessage = new ImageMessage(Role.User, BinaryData.FromBytes(image, mediaType: "image/png"));
+            chatHistory.Add(imageMessage);
+
+            var reply = await _agent.GenerateReplyAsync(chatHistory, options, cancellationToken);
+            if (reply is ToolCallAggregateMessage toolCallMessage)
+            {
+                chatHistory.Add(reply);
+                reply = await _agent.SendAsync(toolCallMessage, chatHistory, ct: cancellationToken);
+            }
+
+            chatHistory.Add(reply);
+            remainingSteps--;
+
+            if (reply.GetContent()?.Contains("```answer") is true)
+            {
+                return reply;
+            }
+        }
+
+        return new TextMessage(Role.Assistant, "I couldn't find the answer in the given steps.", this.Name);
+    }
+
+    // browser action
+
+    [Function]
+    public async Task<string> VisitUrl(string url)
+    {
+        await _page.GotoAsync(url);
+        await DrawAllInteractiveElementsAsync();
+
+        return await _page.TitleAsync();
+    }
+
+    [Function]
+    public async Task<string> HistoryBack()
+    {
+        await _page.GoBackAsync();
+        await DrawAllInteractiveElementsAsync();
+        return await _page.TitleAsync();
+    }
+
+    /// <summary>
+    /// Hit the Enter key on the keyboard.
+    /// </summary>
+    [Function]
+    public async Task<string> Enter()
+    {
+
+       await _page.Keyboard.PressAsync("Enter");
+        await DrawAllInteractiveElementsAsync();
+        return "Enter key pressed";
+    }
+
+    /// <summary>
+    /// Click on an interactive element with the given id. The interactive elements are marked in red rectangles.
+    /// </summary>
+    /// <param name="id">the id of the interactive element, it always located at the left bottom of the element</param>
+    [Function]
+    public async Task<string> Click(int id)
+    {
+        var interactiveElements = await _page.EvaluateAsync("MultimodalWebSurfer.getInteractiveRects();");
+        var str = JsonSerializer.Serialize(interactiveElements);
+        var interactiveRects = JsonSerializer.Deserialize<InteractiveRectangles>(str) ?? throw new Exception("interactiveRects is null");
+        //var interactiveElements = await _page.EvaluateAsync<InteractiveRectangles>("MultimodalWebSurfer.getInteractiveRects();");
+
+
+        //Console.WriteLine(JsonSerializer.Serialize(interactiveElements, new JsonSerializerOptions { WriteIndented = true }));
+        var interactiveElement = interactiveRects.Rects.SelectMany(r => r.Rects).FirstOrDefault(e => e.InteractiveId == id);
+
+        // print all text content
+        foreach (var rect in interactiveRects.Rects.SelectMany(r => r.Rects))
+        {
+            Console.WriteLine(rect.Content);
+        }
+
+        if (interactiveElement == null)
+        {
+            return $"Element with id {id} not found, maybe it's not in the viewport";
+        }
+
+        try
+        {
+            var target = _page.Locator($"[__elementId='{interactiveElement.ElementId}']");
+            await target.ScrollIntoViewIfNeededAsync();
+            await target.ClickAsync();
+
+            await DrawAllInteractiveElementsAsync();
+
+            return $"Clicked on element with id {id}";
+        }
+        catch(PlaywrightException ex) when (ex.Message.Contains("Element is an <input>, <textarea> or [contenteditable] element"))
+        {
+            return $"Element with id {id} is not clickable";
+        }
+        catch (PlaywrightException ex) when (ex.Message.Contains("Element is not visible"))
+        {
+            return $"Element with id {id} is not visible";
+        }
+    }
+
+    [Function]
+    public async Task<string> VisitGoogle()
+    {
+        await _page.GotoAsync("https://www.google.com");
+        // sleep for a while to let the page load
+        await DrawAllInteractiveElementsAsync();
+        return "You are now on Google";
+    }
+
+    [Function]
+    public async Task<string> SearchGoogle(string query)
+    {
+        var encodedQuery = Uri.EscapeDataString(query);
+        await _page.GotoAsync($"https://www.google.com/search?q={encodedQuery}");
+
+        await Task.Delay(2000);
+
+        await DrawAllInteractiveElementsAsync();
+
+        return $"You searched for {query}";
+    }
+
+    [Function]
+    public async Task<string> Type(int id, string text)
+    {
+        var interactiveElements = await _page.EvaluateAsync("MultimodalWebSurfer.getInteractiveRects();");
+        var str = JsonSerializer.Serialize(interactiveElements);
+        var interactiveRects = JsonSerializer.Deserialize<InteractiveRectangles>(str) ?? throw new Exception("interactiveRects is null");
+        var interactiveElement = interactiveRects.Rects.SelectMany(r => r.Rects).FirstOrDefault(e => e.InteractiveId == id);
+
+        if (interactiveElement == null)
+        {
+            return $"Element with id {id} not found, maybe it's not in the viewport";
+        }
+
+        try
+        {
+            var target = _page.Locator($"[__elementId='{interactiveElement.ElementId}']");
+            await target.ScrollIntoViewIfNeededAsync();
+            await target.FillAsync(text);
+
+            await DrawAllInteractiveElementsAsync();
+
+            return $"Typed {text} on element with id {id}";
+        }
+        catch (PlaywrightException ex) when (ex.Message.Contains("Element is not an <input>, <textarea> or [contenteditable] element"))
+        {
+            return $"Element with id {id} is not a text input";
+        }
+    }
+
+    [Function]
+    public async Task<string> PageUp()
+    {
+        await _page.Keyboard.PressAsync("PageUp");
+        await DrawAllInteractiveElementsAsync();
+        return "PageUp";
+    }
+
+    [Function]
+    public async Task<string> PageDown()
+    {
+        await _page.Keyboard.PressAsync("PageDown");
+        await DrawAllInteractiveElementsAsync();
+        return "PageDown";
+    }
+
+    ///// <summary>
+    ///// Convert the current page to markdown format.
+    ///// </summary>
+    //[Function]
+    //public async Task<string> ToMarkdown()
+    //{
+
+    //}
+
+    private async Task DrawAllInteractiveElementsAsync()
+    {
+        var scriptPath = "page_script.js";
+        var script = File.ReadAllText(scriptPath);
+        await _page.EvaluateAsync(script);
+        await _page.EvaluateAsync("MultimodalWebSurfer.drawVisibleInteractiveRects();");
+    }
+}
